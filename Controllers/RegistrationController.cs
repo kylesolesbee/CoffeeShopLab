6ae982614f2f66549522988cbccb43d5dd8d0bcf
Kyle Solesbee <ksolesbee99@gmail.com>
2023-11-02 10:27:12 -0400

    ....Net

---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,25 @@
+﻿using CoffeeShopLab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopLab.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                   
+                return View("Result", user);
+            }
+            return View();
+        }
+    }
+}
